42275a9ebacdbe0db8d7d8a1fd81561072125948
Mickadoodoo <michael.r.marchand@gmail.com>
2022-07-25 21:14:54 -0400

    Major Revision
    - RandomNPC = 1 Class
    - NPC Generator = 1 Class
    - File saves in JSON now instead of pickling.

--- NPC_Generator.py
+++ NPC_Generator.py
@@ -1,9 +1,9 @@
 import random
-import pickle
-from tkinter import filedialog, ttk
+from tkinter import filedialog, messagebox, ttk
 from tkinter import *
-from data import *
+from NPC_data import *
 from datetime import datetime
+import json
 import os
 
 os.chdir(os.path.dirname(__file__))
@@ -35,8 +35,8 @@ class RandomNPC:
         self.build = random.choice(buildList)
         self.pronoun = random.choice(pronounList)
         self.description = random.choice(descriptionList)
-        self.wantsNeeds = random.choice(wantsNeedsList)
-        self.secretsObstacles = random.choice(secretsObstaclesList)
+        self.wants_needs = random.choice(wantsNeedsList)
+        self.secrets_obstacles = random.choice(secretsObstaclesList)
         self.carrying = random.choice(carryingList)
 
     def clear(self):
@@ -62,10 +62,10 @@ class RandomNPC:
             return self.description
 
         elif stat == "WANTSNEEDS":
-            return self.wantsNeeds
+            return self.wants_needs
 
         elif stat == "SECRETS":
-            return self.secretsObstacles
+            return self.secrets_obstacles
 
         elif stat == "CARRYING":
             return self.carrying
@@ -92,26 +92,28 @@ class RandomNPC:
             self.description = random.choice(descriptionList)
 
         elif stat == "WANTSNEEDS":
-            self.wantsNeeds = random.choice(wantsNeedsList)
+            self.wants_needs = random.choice(wantsNeedsList)
 
         elif stat == "SECRETS":
-            self.secretsObstacles = random.choice(secretsObstaclesList)
+            self.secrets_obstacles = random.choice(secretsObstaclesList)
 
         elif stat == "CARRYING":
             self.carrying = random.choice(carryingList)
 
-        elif stat == "ALL":
-            self.firstName = random.choice(firstNameList)
-            self.lastName = random.choice(lastNameList)
-            self.fullName = self.firstName + " " + self.lastName
-            self.age = random.choice(ageList)
-            self.hair = random.choice(hairList)
-            self.build = random.choice(buildList)
-            self.pronoun = random.choice(pronounList)
-            self.description = random.choice(descriptionList)
-            self.wantsNeeds = random.choice(wantsNeedsList)
-            self.secretsObstacles = random.choice(secretsObstaclesList)
-            self.carrying = random.choice(carryingList)
+    def createDict(self):
+        return {
+            "FULLNAME": self.fullName,
+            "AGE": self.age,
+            "HAIR": self.hair,
+            "BUILD": self.build,
+            "PRONOUN": self.pronoun,
+            "DESCRIPTION": self.description,
+            "WANTSNEEDS": self.wants_needs,
+            "SECRETS": self.secrets_obstacles,
+            "CARRYING": self.carrying,
+        }
+
+    # Terminal printing
 
     def printStat(self, stat1, stat_title: str, stat2=""):
         print("----" + stat_title.center(TERMINAL_SPACING) + "----")
@@ -131,251 +133,188 @@ class RandomNPC:
         self.printStat(self.hair, "Hair")
         self.printStat(self.build, "Build")
         self.printStat(self.description, "Little Description")
-        self.printStat(self.wantsNeeds, "Wants / Needs")
-        self.printStat(self.secretsObstacles, "Secret / Obstacle")
+        self.printStat(self.wants_needs, "Wants / Needs")
+        self.printStat(self.secrets_obstacles, "Secret / Obstacle")
         self.printStat(self.carrying, "Items Carried")
 
 
-def createFrames(root, w, h, rel: str = None):
-    return ttk.Frame(root, width=w, height=h, relief=rel)
+class NPCGeneratorWindow:
+    def __init__(self, master: Tk):
+        def createTextbox(root, NPC: RandomNPC, stat, width, height):
+            textbox = Text(root, width=width, height=height)
+            textbox.insert(1.0, NPC.callStat(stat))
+            textbox.config(wrap="word", state="disabled")
+            return textbox
+
+        def createLabel(root, stat: str):
+            if stat == "FULLNAME":
+                return ttk.Label(root, text="Name: ")
+
+            elif stat == "AGE":
+                return ttk.Label(root, text="Age: ")
+
+            elif stat == "HAIR":
+                return ttk.Label(root, text="Hair: ")
+
+            elif stat == "BUILD":
+                return ttk.Label(root, text="Build: ")
+
+            elif stat == "PRONOUN":
+                return ttk.Label(root, text="Pronoun: ")
+
+            elif stat == "DESCRIPTION":
+                return ttk.Label(root, text="Description: ")
+
+            elif stat == "WANTSNEEDS":
+                return ttk.Label(root, text="Wants/Needs: ")
+
+            elif stat == "SECRETS":
+                return ttk.Label(root, text="Secrets/Obstacles: ")
+
+            elif stat == "CARRYING":
+                return ttk.Label(root, text="Carrying: ")
+
+        def rerollStat(NPC: RandomNPC, stat: str, textbox: Text):
+            NPC.rerollStats(stat)
+            textbox.config(state="normal")
+            textbox.replace(1.0, "end", NPC.callStat(stat))
+            textbox.config(state="disabled")
+
+        def rerollAll(NPC: RandomNPC, textbox_dict: dict):
+            for stat in textbox_dict:
+                rerollStat(NPC, stat, textbox_dict[stat])
+
+        def createItemLine(root, NPC, stat: str, row_index):
+            textbox = createTextbox(root, self.NPC, stat, 30, 3)
+            button = ttk.Button(
+                root,
+                text=" Re-Roll",
+                image=self.diceIcon,
+                compound=LEFT,
+                command=lambda: rerollStat(NPC, stat, textbox),
+            )
+            label = createLabel(root, stat)
+            button.grid(row=row_index, column=0)
+            label.grid(row=row_index, column=1)
+            textbox.grid(row=row_index, column=2)
+            return textbox
+
+        def saveNPC():
+            save_path = os.path.join((os.path.dirname(__file__)), "Saved_Files")
+            isExist = os.path.exists(save_path)
+            if not isExist:
+                os.makedirs(save_path)
+
+            file_name = f"{self.NPC.fullName}.json"
+            with open(os.path.join(save_path, file_name), "w") as file:
+                json.dump(self.NPC.createDict(), file)
+            messagebox.showinfo(
+                title="NPC Generator Save",
+                message=f'"{self.NPC.fullName}" successfully saved!',
+            )
+
+        def loadNPC(textbox_dict):
+            file_name = filedialog.askopenfile().name
+            with open(file_name, "r") as file:
+                loaded_NPC: dict = json.load(file)
+
+            for k, v in loaded_NPC.items():
+                textbox_dict[k].config(state="normal")
+                textbox_dict[k].replace(1.0, "end", loaded_NPC[k])
+                textbox_dict[k].config(state="disabled")
+
+        self.NPC = RandomNPC()
+        self.NPC_frame = ttk.Frame(master)
+        self.NPC_frame.pack()
+        self.diceIcon = PhotoImage(file="assets/dice.gif")
+
+        # Window dimensions and creation
+        master.title("NPC Generator")
+        master.geometry(WINDOW_SIZE)
+        master.resizable(False, False)
+
+        """
+        Fill with content
+        """
+        self.full_name_text = createItemLine(self.NPC_frame, self.NPC, "FULLNAME", 1)
+        self.age_text = createItemLine(self.NPC_frame, self.NPC, "AGE", 2)
+        self.pronoun_text = createItemLine(self.NPC_frame, self.NPC, "PRONOUN", 3)
+        self.hair_text = createItemLine(self.NPC_frame, self.NPC, "HAIR", 4)
+        self.build_text = createItemLine(self.NPC_frame, self.NPC, "BUILD", 5)
+        self.description_text = createItemLine(
+            self.NPC_frame, self.NPC, "DESCRIPTION", 6
+        )
+        self.wants_needs_text = createItemLine(
+            self.NPC_frame, self.NPC, "WANTSNEEDS", 7
+        )
+        self.secrets_text = createItemLine(self.NPC_frame, self.NPC, "SECRETS", 8)
+        self.carrying_text = createItemLine(self.NPC_frame, self.NPC, "CARRYING", 9)
+
+        self.textbox_dict = {
+            "FULLNAME": self.full_name_text,
+            "AGE": self.age_text,
+            "PRONOUN": self.pronoun_text,
+            "HAIR": self.hair_text,
+            "BUILD": self.build_text,
+            "DESCRIPTION": self.description_text,
+            "WANTSNEEDS": self.wants_needs_text,
+            "SECRETS": self.secrets_text,
+            "CARRYING": self.carrying_text,
+        }
+        self.reroll_all_button = ttk.Button(
+            self.NPC_frame,
+            text="Re-Roll Everything",
+            image=self.diceIcon,
+            compound=LEFT,
+            command=lambda: rerollAll(self.NPC, self.textbox_dict),
+        )
+        self.reroll_all_button.grid(row=0, column=0, columnspan=3)
+
+        # Menu option creation
+        master.option_add("*tearOff", False)
+        self.menubar = Menu(master)
+        master.config(menu=self.menubar)
+        self.file = Menu(self.menubar)
+        self.menubar.add_cascade(menu=self.file, label="File")
+        self.file.add_command(label="Save", command=lambda: saveNPC())
+        self.file.add_command(
+            label="Load...", command=lambda: loadNPC(self.textbox_dict)
+        )
 
 
-def createEntry(NPC: RandomNPC, frame, e_width, stat):
-    entry = ttk.Entry(frame, width=e_width)
-    entry.insert(0, NPC.callStat(stat))
-    entry.state(["readonly"])
-    return entry
-
-
-def rerollButton(parent, icon, pressFunction, text=" Re-Roll"):
-    return ttk.Button(
-        parent, text=text, image=icon, compound=LEFT, command=pressFunction
-    )
-
-
-def createLabel(parent, stat):
-    if stat == "FULLNAME":
-        return ttk.Label(parent, text="Name: ")
-
-    elif stat == "AGE":
-        return ttk.Label(parent, text="Age: ")
-
-    elif stat == "HAIR":
-        return ttk.Label(parent, text="Hair: ")
-
-    elif stat == "BUILD":
-        return ttk.Label(parent, text="Build: ")
-
-    elif stat == "PRONOUN":
-        return ttk.Label(parent, text="Pronoun: ")
-
-    elif stat == "DESCRIPTION":
-        return ttk.Label(parent, text="Description: ")
-
-    elif stat == "WANTSNEEDS":
-        return ttk.Label(parent, text="Wants/Needs: ")
-
-    elif stat == "SECRETS":
-        return ttk.Label(parent, text="Secrets/Obstacles: ")
-
-    elif stat == "CARRYING":
-        return ttk.Label(parent, text="Carrying: ")
-
-
-def rerollStat(NPC: RandomNPC, stat: str, entry: ttk.Entry):
-    NPC.rerollStats(stat)
-    entry.state(["!readonly"])
-    entry.delete(0, END)
-    entry.insert(0, NPC.callStat(stat))
-    entry.state(["readonly"])
-
-
-def rerollAll(NPC: RandomNPC, entryDict: dict):
-    for stat in entryDict:
-        NPC.rerollStats(stat)
-        entryDict[stat].state(["!readonly"])
-        entryDict[stat].delete(0, END)
-        entryDict[stat].insert(0, NPC.callStat(stat))
-        entryDict[stat].state(["readonly"])
-
-
-def buildFrameGroup(
-    r,
-    labelFrame,
-    diceFrame,
-    infoFrame,
-    NPC: RandomNPC,
-    stat: str,
-    e_width,
-):
-    entry = createEntry(NPC, infoFrame, e_width, stat)
-    dice = rerollButton(diceFrame, diceIcon, lambda: rerollStat(NPC, stat, entry))
-    label = createLabel(labelFrame, stat)
-    dice.pack(fill="both", expand=True)
-    label.pack(fill="both", expand=True)
-    return entry
-
-
-# small save to .txt
-def exportNPCToText(NPC: RandomNPC):
-    savePath = os.path.join((os.path.dirname(__file__)), "Saved_Files")
-    isExist = os.path.exists(savePath)
-    if not isExist:
-        os.makedirs(savePath)
-
-    savePath = os.path.join(savePath, "NPC Exported Text")
-    isExist = os.path.exists(savePath)
-    if not isExist:
-        os.makedirs(savePath)
-
-    character_info = f"""Character Name: {NPC.fullName}
-Age: {NPC.age}
-Hair: {NPC.hair}
-Build: {NPC.build}
-Gender: {NPC.pronoun}
-
-Description: {NPC.description}
-
-Wants/Needs: {NPC.wantsNeeds}
-
-Secrets/Obstacle: {NPC.secretsObstacles}
-
-Carrying: {NPC.carrying}"""
-
-    date = datetime.now().strftime("%Y-%m-%d-%H%M%S")
-    title_name = f"{NPC.firstName}-{NPC.lastName}_{date}.txt"
-
-    with open(os.path.join(savePath, title_name), "w") as f:
-        f.write(character_info)
-
-
-def pickleNPC(NPC: RandomNPC):
-    savePath = os.path.join((os.path.dirname(__file__)), "Saved_Files")
-    isExist = os.path.exists(savePath)
-    if not isExist:
-        os.makedirs(savePath)
-
-    file_name = f"{NPC.fullName}.npc"
-    with open(os.path.join(savePath, file_name), "wb") as file:
-        pickle.dump(NPC, file)
-
-
-def loadNPC(NPC: RandomNPC, entryDict: dict):
-    file_name = filedialog.askopenfile().name
-    with open(file_name, "rb") as file:
-        loaded_NPC = pickle.load(file)
-
-    NPC = loaded_NPC
-    for stat in entryDict:
-        entryDict[stat].state(["!readonly"])
-        entryDict[stat].delete(0, END)
-        entryDict[stat].insert(0, NPC.callStat(stat))
-        entryDict[stat].state(["readonly"])
-
-
-"""
-main body
-"""
-
-NPC = RandomNPC()
-
-
-# main display
 root = Tk()
-root.geometry(WINDOW_SIZE)
-root.resizable(False, False)
-root.title("NPC Generator")
-
-topFrame = createFrames(root, WINDOW_WIDTH, WINDOW_HEIGHT // 10)
-topFrame.pack()
-panedwindow = ttk.Panedwindow(root, orient=HORIZONTAL)
-panedwindow.pack(fill=BOTH, expand=True)
-diceFrame = ttk.Frame(panedwindow, width=100, height=800)
-labelFrame = ttk.Frame(panedwindow, width=50, height=400)
-infoFrame = ttk.Frame(panedwindow, width=400, height=400)
-panedwindow.add(diceFrame)
-panedwindow.add(labelFrame)
-panedwindow.add(infoFrame, weight=4)
-
-# menu
-root.option_add("*tearOff", False)
-menubar = Menu(root)
-root.config(menu=menubar)
-file = Menu(menubar)
-menubar.add_cascade(menu=file, label="File")
-file.add_command(label="Save", command=lambda: pickleNPC(NPC))
-file.add_command(label="Load...", command=lambda: loadNPC(NPC, entryDict))
-file.add_command(label="Export as .txt file", command=lambda: exportNPCToText(NPC))
-
-# assets
-diceIcon = PhotoImage(file="assets/dice.gif")
-
-"""
-Build the display
-"""
-
-# FULLNAME
-fullNameEntry = buildFrameGroup(
-    0, labelFrame, diceFrame, infoFrame, NPC, "FULLNAME", 15
-)
-fullNameEntry.pack(fill="both", expand=True)
-
-# AGE
-ageEntry = buildFrameGroup(1, labelFrame, diceFrame, infoFrame, NPC, "AGE", 15)
-ageEntry.pack(fill="both", expand=True)
-
-# PRONOUN
-pronounEntry = buildFrameGroup(2, labelFrame, diceFrame, infoFrame, NPC, "PRONOUN", 15)
-pronounEntry.pack(fill="both", expand=True)
-
-# HAIR
-hairEntry = buildFrameGroup(3, labelFrame, diceFrame, infoFrame, NPC, "HAIR", 15)
-hairEntry.pack(fill="both", expand=True)
-
-# BUILD
-buildEntry = buildFrameGroup(4, labelFrame, diceFrame, infoFrame, NPC, "BUILD", 15)
-buildEntry.pack(fill="both", expand=True)
-
-# DESCRIPTION
-descriptionEntry = buildFrameGroup(
-    5, labelFrame, diceFrame, infoFrame, NPC, "DESCRIPTION", 15
-)
-descriptionEntry.pack(fill="both", expand=True)
-
-# WANTSNEEDS
-wantsNeedsEntry = buildFrameGroup(
-    6, labelFrame, diceFrame, infoFrame, NPC, "WANTSNEEDS", 15
-)
-wantsNeedsEntry.pack(fill="both", expand=True)
-
-# SECRETS
-secretsEntry = buildFrameGroup(7, labelFrame, diceFrame, infoFrame, NPC, "SECRETS", 15)
-secretsEntry.pack(fill="both", expand=True)
-
-# CARRYING
-carryingEntry = buildFrameGroup(
-    10, labelFrame, diceFrame, infoFrame, NPC, "CARRYING", 15
-)
-carryingEntry.pack(fill="both", expand=True)
-
-# REROLL ALL
-entryDict = {
-    "FULLNAME": fullNameEntry,
-    "AGE": ageEntry,
-    "PRONOUN": pronounEntry,
-    "HAIR": hairEntry,
-    "BUILD": buildEntry,
-    "DESCRIPTION": descriptionEntry,
-    "WANTSNEEDS": wantsNeedsEntry,
-    "SECRETS": secretsEntry,
-    "CARRYING": carryingEntry,
-}
-allDice = rerollButton(
-    topFrame,
-    diceIcon,
-    lambda: rerollAll(NPC, entryDict),
-    text="Re-Roll Everything",
-)
-allDice.pack()
+npc_generator = NPCGeneratorWindow(root)
 
 root.mainloop()
+
+# # small save to .txt
+# def exportNPCToText(NPC: RandomNPC):
+#     savePath = os.path.join((os.path.dirname(__file__)), "Saved_Files")
+#     isExist = os.path.exists(savePath)
+#     if not isExist:
+#         os.makedirs(savePath)
+
+#     savePath = os.path.join(savePath, "NPC Exported Text")
+#     isExist = os.path.exists(savePath)
+#     if not isExist:
+#         os.makedirs(savePath)
+
+#     character_info = f"""Character Name: {NPC.fullName}
+# Age: {NPC.age}
+# Hair: {NPC.hair}
+# Build: {NPC.build}
+# Gender: {NPC.pronoun}
+
+# Description: {NPC.description}
+
+# Wants/Needs: {NPC.wants_needs}
+
+# Secrets/Obstacle: {NPC.secrets_obstacles}
+
+# Carrying: {NPC.carrying}"""
+
+#     date = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+#     title_name = f"{NPC.firstName}-{NPC.lastName}_{date}.txt"
+
+#     with open(os.path.join(savePath, title_name), "w") as f:
+#         f.write(character_info)
