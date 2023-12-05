0980cbf3f71f12e3e7f5fb9d3c75084462632ea9
MickMarch <michael.r.marchand@gmail.com>
2023-12-04 21:51:18 -0500

    update dataclasses

--- npc_data_classes.py
+++ npc_data_classes.py
@@ -1,4 +1,15 @@
 import random
+import json
+
+
+class NpcAttribute:
+    def __init__(self, name, attribute_list: list[str]):
+        self.name = name
+        self.attribute_list = attribute_list
+        self.get_new_attribute()
+
+    def get_new_attribute(self) -> None:
+        self.current_attribute = random.choice(self.attribute_list)
 
 
 class NpcData:
