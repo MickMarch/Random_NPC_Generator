77b1376b6acc8d88ff049c09dfc6bc9cb689a00f
MickMarch <michael.r.marchand@gmail.com>
2023-12-04 21:13:02 -0500

    update view.py

--- view.py
+++ view.py
@@ -2,8 +2,33 @@ import tkinter as tk
 
 from model import Model
 
+TITLE = "NPC Generator"
+WINDOW_HEIGHT = 530
+WINDOW_WIDTH = 600
+WINDOW_X_SHIFT = 350
+WINDOW_Y_SHIFT = 80
+DIMENSIONS = (
+    str(WINDOW_WIDTH)
+    + "x"
+    + str(WINDOW_HEIGHT)
+    + "+"
+    + str(WINDOW_X_SHIFT)
+    + "+"
+    + str(WINDOW_Y_SHIFT)
+)
+
 
 class View(tk.Tk):
-    def __init__(self, model: Model):
+    def __init__(self, model: Model) -> None:
         super().__init__()
         self.model = model
+        self.title = TITLE
+        self.dimensions = DIMENSIONS
+        self.create_ui()
+        self.update_npc()
+
+    def create_ui(self) -> None:
+        pass
+
+    def update_npc(self) -> None:
+        pass
